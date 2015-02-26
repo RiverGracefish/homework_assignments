5d4e371761ed622880d8a92d3ce0d8bcb18d0b89
Grace Yu <grace.yu4444@gmail.com>
2015-02-25 17:59:44 -0700

    make it excitable

--- objects/cash_register.rb
+++ objects/cash_register.rb
@@ -13,33 +13,39 @@ class CashRegister
     @total.round(2)
   end
 
+  def purchase(amount)
+    @puchase = amount.to_f
+    @total = (@total + @puchase).round(2)
+  end
+
   def pay(payment)
     pay = payment.to_f
     new_total = @total - pay
-    @total = 0
+    @total -= pay
     if @total < 0
       @total = 0
+      - new_total.round(2)
     else
-      @total = new_total
+      @total.round(2)
     end
   end
 end
 
 # Example Register transaction
 register = CashRegister.new
-register.total  # => 0.00
-register.purchase(3.78)  # => 3.78
-register.total  # => 3.78
-register.pay(5.00)  # => "Your change is $1.22"
-register.total # => 0.00
+puts register.total  # => 0.00
+puts register.purchase(3.78)  # => 3.78
+puts register.total  # => 3.78
+puts register.pay(5.00)  # => "Your change is $1.22"
+puts register.total # => 0.00
 
 # Example Register transaction #2
-# register = CashRegister.new
-# register.total  # => 0.00
-# register.purchase(3.78)  	# => 3.78
-# register.purchase(5.22)	# => 9.00
-# register.total  			# => 9.00
-# register.pay(5.00)  # => "Your new total is $4.00"
-# register.total			# => 4.00
-# register.pay(5.00)  # => "Your change is $1.00"
-# register.total # => 0.00
+register = CashRegister.new
+puts register.total  # => 0.00
+puts register.purchase(3.78)   # => 3.78
+puts register.purchase(5.22)   # => 9.00
+puts register.total  # => 9.00
+puts register.pay(5.00)  # => "Your new total is $4.00"
+puts register.total  # => 4.00
+puts register.pay(5.00)  # => "Your change is $1.00"
+puts register.total # => 0.00
