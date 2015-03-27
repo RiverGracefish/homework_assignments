ff6cffdf5298fb69154b891d46aaf12e653cdc6f
Grace Yu <grace.yu4444@gmail.com>
2015-03-26 21:54:53 -0600

    took care of DRY
    [#89949804]

--- objects_tdd/spec/lib/schedule_objects_spec.rb
+++ objects_tdd/spec/lib/schedule_objects_spec.rb
@@ -1,10 +1,10 @@
 require_relative '../../lib/schedule_objects.rb'
 
 describe Schedule do
-  # it 'should be a schedule' do
-  #   expect(subject).to be_a(Schedule)
-  # end
-  #
+  before(:each) do
+    @schedule = Schedule.new(Time.now)
+  end
+
   it 'should initialize datetime' do
     datetime = Time.now
     schedule = Schedule.new(datetime)
@@ -13,39 +13,33 @@ describe Schedule do
 
   describe '#work_days' do
     it 'should return array of work days' do
-      schedule = Schedule.new(Time.now)
-      expect(schedule.work_days).to eq(['Mon', 'Tue', 'Wed', 'Thur', 'Fri'])
+      expect(@schedule.work_days).to eq(['Mon', 'Tue', 'Wed', 'Thur', 'Fri'])
     end
   end
 
   describe '#school_days' do
     it 'should return array of school days' do
-      schedule = Schedule.new(Time.now)
-      expect(schedule.school_days).to eq(['Mon', 'Wed', 'Fri'])
+      expect(@schedule.school_days).to eq(['Mon', 'Wed', 'Fri'])
     end
   end
 
   describe '#work_on_day?' do
     it 'should return true if work on day' do
-      schedule = Schedule.new(Time.now)
-      expect(schedule.work_on_day?('Wed')).to be_truthy
+      expect(@schedule.work_on_day?('Wed')).to be_truthy
     end
 
     it 'should return false if no work on day' do
-      schedule = Schedule.new(Time.now)
-      expect(schedule.work_on_day?('Sun')).to be_falsey
+      expect(@schedule.work_on_day?('Sun')).to be_falsey
     end
   end
 
   describe '#school_on_day?' do
     it 'should return true if school on day' do
-      schedule = Schedule.new(Time.now)
-      expect(schedule.school_on_day?('Fri')).to be_truthy
+      expect(@schedule.school_on_day?('Fri')).to be_truthy
     end
 
     it 'should return false if no school on day' do
-      schedule = Schedule.new(Time.now)
-      expect(schedule.school_on_day?('Tue')).to be_falsey
+      expect(@schedule.school_on_day?('Tue')).to be_falsey
     end
   end
 end
